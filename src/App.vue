679085beeb0e3886b095a413e273dd4c683aa198
volodymyr <kokolus7806@gmail.com>
2020-08-25 18:39:23 +0300

    create task2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <AddTodo
+    @addtodo="addtodo"
+    :todoarray ="todoarray"
+    />
+    <TodoList
+        :todoarray ="todoarray"
+        @deleteitem = 'deleteitem'
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import AddTodo from "@/components/AddTodo";
+import TodoList from "@/components/Frameworks/TodoList";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddTodo,
+    TodoList,
+  },
+  data() {
+    return {
+      todoarray: [],
+       }
+  },
+  methods: {
+    addtodo(event) {
+      this.todoarray.push(event);
+    },
+    deleteitem(event) {
+
+      for (let i = 0; i < this.todoarray.length; i++){
+        if (this.todoarray[i] === event){
+          this.todoarray.splice(i, 1)
+      }}
+
+    },
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
